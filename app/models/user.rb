e30a016c6e4b2a41d1ce791178759894502e9428
maxeskell <maxeskell1@gmail.com>
2017-07-21 11:52:16 +0100

    backend coimplete

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   has_many :ideas
   has_many :ideas_loved, class_name: "Idea", join_table: "ideas_users_love"
   has_many :ideas_joined, class_name: "Idea", join_table: "ideas_users_join"
-  has_secure_password
+  has_secure_password validations: false
 
   validates :username, presence: true
   validates :email, presence: true, uniqueness: true, unless: :oauth_login?
   validates :password, presence: true, confirmation: true, unless: :oauth_login?, on: :create
 
   def oauth_login?
-    github_id.present?
+    github_id.present? || facebook_id.present?
   end
  end
