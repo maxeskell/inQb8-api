45d80467c03462f65efeabbeca38ef489f5e669f
maxeskell <maxeskell1@gmail.com>
2017-07-26 11:26:47 +0100

    facebook attempt to fix 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   has_secure_password validations: false
 
   validates :username, presence: true
-  validates :email, presence: true, uniqueness: true, unless: :oauth_login?, on: :create
-  validates :password, presence: true, confirmation: true, unless: :oauth_login?, on: :create
+  validates :email, presence: true, uniqueness: true, unless: :oauth_login?
+  validates :password, presence: true, confirmation: true, unless: :oauth_login?
 
   def oauth_login?
     github_id.present? || facebook_id.present?
