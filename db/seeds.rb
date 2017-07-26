daf0571ec2127f30dd9fa26a6f05f7327fb8023f
maxeskell <maxeskell1@gmail.com>
2017-07-26 11:01:23 +0100

    facebook attempt to fix 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,8 +34,8 @@ users = User.create!([
 ideas = Idea.create!([
                        { title: 'new code language', pitch: "let's create a new code which does everything and is great", description: 'We will create a new langaue that will rule the world!!!', remote_image_url: 'https://tctechcrunch2011.files.wordpress.com/2015/04/codecode.jpg?w=738', campaign_duration: '80', who_we_need: 'two developers, two designers', benefits_for_you: 'happiness', user: users[0], tags: tags[1,2], target: 4},
                        { title: 'otters 4 everyone', pitch: 'lets make the world a better place with otters', description: 'Issue everyone with 1 x otters and supporting equipment', remote_image_url: 'https://images7.alphacoders.com/355/thumb-1920-355113.jpg', campaign_duration: '90', who_we_need: '7.2 billion otters', benefits_for_you: 'otteryness', user: users[1], tags: tags[2,3], target: 5 },
-                       { title: 'green travel', pitch: "let's reduce our carbon footprint by 50%", description: 'We are the thrid highest user of airtravel in the world.  A 50% reduction would save thousands of tones of CO2', remote_image_url: 'https://d3gxp3iknbs7bs.cloudfront.net/media/618/364a3506-8edc-427d-92be-2667e6c872a1.jpg', campaign_duration: '20', who_we_need: 'three people to help brainstrom and get stuff done', benefits_for_you: 'a chance to be involved in a great project for the firm and the environment', user: users[1], tags: tags[1,5], target: 3 },
-                       { title: 'AI for hiring', pitch: 'lets create a new code which does everything and is great', description: 'We will create a new langaue that will rule the world!!!', remote_image_url: 'http://www.impactlab.net/wp-content/uploads/2016/02/ai-image.jpg', campaign_duration: '80', who_we_need: 'two developers, two designers', benefits_for_you: 'happiness', user: users[2], tags: tags[3,4], target: 7 }
+                       { title: 'green travel', pitch: "let's reduce our carbon footprint by 50%", description: 'We are the thrid highest user of airtravel in the world.  A 50% reduction would save thousands of tones of CO2', remote_image_url: 'https://d3gxp3iknbs7bs.cloudfront.net/media/618/364a3506-8edc-427d-92be-2667e6c872a1.jpg', campaign_duration: '20', who_we_need: 'three people to help brainstrom and get stuff done', benefits_for_you: 'a chance to be involved in a great project for the firm and the environment', user: users[2], tags: tags[1,5], target: 3 },
+                       { title: 'AI for hiring', pitch: 'lets create a new code which does everything and is great', description: 'We will create a new langaue that will rule the world!!!', remote_image_url: 'http://www.impactlab.net/wp-content/uploads/2016/02/ai-image.jpg', campaign_duration: '80', who_we_need: 'two developers, two designers', benefits_for_you: 'happiness', user: users[3], tags: tags[3,4], target: 7 }
                      ])
 
 comments = Comment.create!([{
@@ -50,20 +50,20 @@ comments = Comment.create!([{
 
 
 
-ideas[0].loves << users[2]
 ideas[0].loves << users[3]
-ideas[0].loves << users[1]
+ideas[1].loves << users[3]
+ideas[0].loves << users[2]
 ideas[1].loves << users[2]
-ideas[2].loves << users[0]
-ideas[2].loves << users[3]
-ideas[3].loves << users[0]
+ideas[0].loves << users[1]
 ideas[3].loves << users[1]
+ideas[2].loves << users[0]
+ideas[3].loves << users[0]
 
-ideas[0].joiners << users[0]
-ideas[0].joiners << users[1]
-ideas[1].joiners << users[1]
+ideas[0].joiners << users[3]
+ideas[1].joiners << users[3]
 ideas[1].joiners << users[2]
 ideas[2].joiners << users[2]
-ideas[2].joiners << users[3]
-ideas[3].joiners << users[2]
-ideas[3].joiners << users[3]
+ideas[3].joiners << users[1]
+ideas[2].joiners << users[1]
+ideas[2].joiners << users[0]
+ideas[1].joiners << users[0]
