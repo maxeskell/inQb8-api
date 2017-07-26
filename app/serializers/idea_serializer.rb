3793be9222ab9149a7010d0ce9de8a289eacd1f4
maxeskell <maxeskell1@gmail.com>
2017-07-26 16:28:10 +0100

    Updated form validation

--- app/serializers/idea_serializer.rb
+++ app/serializers/idea_serializer.rb
@@ -1,5 +1,5 @@
 class IdeaSerializer < ActiveModel::Serializer
-  attributes :id, :title, :pitch, :description, :image_src, :campaign_duration, :who_we_need, :benefits_for_you, :created_at, :end_date, :days_left, :comments, :tags, :target, :loves, :joiners, :love_ids, :joiner_ids
+  attributes :id, :title, :pitch, :description, :image_src, :campaign_duration, :who_we_need, :benefits_for_you, :created_at, :end_date, :days_left, :comments, :tags, :target, :loves, :joiners, :love_ids, :joiner_ids, :people_left
 
   belongs_to :user
   has_many :comments
@@ -19,6 +19,10 @@ class IdeaSerializer < ActiveModel::Serializer
 
   def days_left
     (end_date - Time.current).to_i / (24 * 60 * 60)
+  end
+
+  def people_left
+    object.target - object.joiners.length
   end
 
 end
