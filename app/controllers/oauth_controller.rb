e45e5df5936c7b4e6141f877168bb559e0479f3e
maxeskell <maxeskell1@gmail.com>
2017-07-26 13:53:21 +0100

    Added logging to oauth controller

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -35,7 +35,7 @@ class OauthController < ApplicationController
         token = HTTParty.post('https://graph.facebook.com/v2.8/oauth/access_token', {
           query: {
             client_id: ENV["FB_OAUTH_INCUBATE_ID"],
-            redirect_uri: ENV["APP_URL"] + '/' || "http://localhost:7000/",
+            redirect_uri: ENV["APP_URL"] << "/" || "http://localhost:7000/",
             client_secret: ENV["FB_OAUTH_INCUBATE_SECRET"],
             code: params[:code]
           },
@@ -55,7 +55,7 @@ class OauthController < ApplicationController
             p profile
 
 
-            user = User.where(facebook_id: profile["id"]).first
+            user = User.where("email = :email OR facebook_id = :facebook_id", email: profile["email"], facebook_id: profile["id"]).first
 
             user = User.new username: profile["name"], email: profile["email"] unless user
 
